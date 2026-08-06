48dc44fff91a3121e644c2136485e77f202f8ecd
malitha-tishamal <malithatishamal@gmail.com>
2026-08-06 22:27:57 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
+import { extendedConfig } from "./src/utils/extendedConfig";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        darkBg: "#07090e",
-      },
+    ...extendedConfig
     },
   },
   plugins: [],
 };
-
 export default config;

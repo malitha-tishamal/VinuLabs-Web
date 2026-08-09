f63dca7c0ff8ef6b65dc2587b3116b991cbdcf2f
malitha-tishamal <malithatishamal@gmail.com>
2026-08-09 17:33:27 +0530

    Update page.tsx

--- src/app/(site)/blog/page.tsx
+++ src/app/(site)/blog/page.tsx
@@ -13,6 +13,8 @@ const BlogPage = () => {
         title='Blog'
         description='Discover a wealth of insightful materials meticulously crafted to provide you with a comprehensive understanding of the latest trends.'
         breadcrumbLinks={breadcrumbLinks}
+        titleKey='blogTitle'
+        descKey='blogDesc'
       />
       <BlogList />
     </>

0149630537337f7159cb9215fbab2ae8fcd3905c
malitha-tishamal <malithatishamal@gmail.com>
2026-08-09 17:33:25 +0530

    Update page.tsx

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -19,6 +19,8 @@ const page = () => {
         title="Contact Us"
         description="Let's Build the Future Together. Have a project idea? Need a partner for AI, IoT, or enterprise systems? Our team is ready to help you transform your vision into reality."
         breadcrumbLinks={breadcrumbLinks}
+        titleKey="contactPageTitle"
+        descKey="contactPageDesc"
       />
       <ContactInfo />
       <ContactForm />

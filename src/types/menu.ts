d07adcf74918d54ef803f5898abdfe138bfa5cd5
malitha-tishamal <malithatishamal@gmail.com>
2026-08-09 17:33:05 +0530

    Update menu.ts

--- src/types/menu.ts
+++ src/types/menu.ts
@@ -1,10 +1,12 @@
 export type SubmenuItem = {
     label: string;
     href: string;
+    key?: string;
   };    
   
   export type HeaderItem = {
     label: string;
     href: string;
+    key?: string;
     submenu?: SubmenuItem[];
   };
